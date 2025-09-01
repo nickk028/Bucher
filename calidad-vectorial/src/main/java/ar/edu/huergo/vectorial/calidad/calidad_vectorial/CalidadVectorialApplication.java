bea9bc7b191e1375fb2870afc18b518525f40f40
IgnPine <ipineda@alumno.huergo.edu.ar>
2025-09-01 20:48:18 -0300

    Creacion proyecto

---
+++ calidad-vectorial/src/main/java/ar/edu/huergo/vectorial/calidad/calidad_vectorial/CalidadVectorialApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.huergo.vectorial.calidad.calidad_vectorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalidadVectorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalidadVectorialApplication.class, args);
+	}
+
+}
---
+++ calidad-vectorial/src/test/java/ar/edu/huergo/vectorial/calidad/calidad_vectorial/CalidadVectorialApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.huergo.vectorial.calidad.calidad_vectorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalidadVectorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
