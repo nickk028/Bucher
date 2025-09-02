62df4b284d308c4b476103bb25f832193acc9e71
IgnPine <ipineda@alumno.huergo.edu.ar>
2025-09-01 21:36:47 -0300

    creacion rol y usuario y repositoris

---
+++ calidad-vectorial/src/main/java/ar/edu/huergo/vectorial/calidad/bucher/entity/security/Rol.java
@@ -0,0 +1,30 @@
+package ar.edu.huergo.vectorial.calidad.bucher.entity.security;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "roles")
+public class Rol {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String nombre; // Ej: ADMIN, CLIENTE
+
+    public Rol(String nombre) {
+        this.nombre = nombre;
+    }
+}
---
+++ calidad-vectorial/src/main/java/ar/edu/huergo/vectorial/calidad/bucher/repository/security/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package ar.edu.huergo.vectorial.calidad.bucher.repository.security;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import ar.edu.huergo.vectorial.calidad.bucher.entity.security.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByUsername(String username);
+    boolean existsByUsername(String username);
+}
