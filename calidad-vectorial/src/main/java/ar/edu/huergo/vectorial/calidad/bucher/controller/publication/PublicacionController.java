7b3aa346db6bed7058226446c0d81ed952a42d5d
IgnPine <ipineda@alumno.huergo.edu.ar>
2025-09-08 15:27:41 -0300

    implementacion de creacion de prestamos

--- calidad-vectorial/src/main/java/ar/edu/huergo/vectorial/calidad/bucher/controller/publication/PublicacionController.java
+++ calidad-vectorial/src/main/java/ar/edu/huergo/vectorial/calidad/bucher/controller/publication/PublicacionController.java
@@ -2,7 +2,10 @@ package ar.edu.huergo.vectorial.calidad.bucher.controller.publication;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,9 +15,27 @@ import org.springframework.web.bind.annotation.RestController;
 import lombok.RequiredArgsConstructor;
 import jakarta.validation.Valid;
 
+import ar.edu.huergo.vectorial.calidad.bucher.dto.publication.PublicacionCreateDTO;
+import ar.edu.huergo.vectorial.calidad.bucher.dto.publication.PublicacionResponseDTO;
+import ar.edu.huergo.vectorial.calidad.bucher.entity.publication.Publicacion;
+import ar.edu.huergo.vectorial.calidad.bucher.mapper.publication.PublicacionMapper;
+import ar.edu.huergo.vectorial.calidad.bucher.service.publication.PublicacionService;
+
 @RestController
-@RequiredArgsConstructor
 @RequestMapping("/publicacion")
 public class PublicacionController {
-    
+
+    @Autowired PublicacionService publicacionService;
+
+    @Autowired PublicacionMapper publicacionMapper;
+
+    @PostMapping
+    public ResponseEntity<PublicacionResponseDTO> crearPublicacion(@RequestBody PublicacionCreateDTO publicacionCreateDTO,
+    @AuthenticationPrincipal UserDetails usuarioAutenticado) {
+
+        Publicacion publicacionNueva = publicacionMapper.toEntity(publicacionCreateDTO);
+        Publicacion publicacionCreada = publicacionService.crearPublicacion(publicacionNueva, publicacionCreateDTO.getTitulo(), usuarioAutenticado.getUsername());
+
+        return ResponseEntity.ok(publicacionMapper.toDTO(publicacionCreada));
+    }
 }
--- calidad-vectorial/src/main/java/ar/edu/huergo/vectorial/calidad/bucher/mapper/publication/PublicacionMapper.java
+++ calidad-vectorial/src/main/java/ar/edu/huergo/vectorial/calidad/bucher/mapper/publication/PublicacionMapper.java
@@ -1,5 +1,40 @@
 package ar.edu.huergo.vectorial.calidad.bucher.mapper.publication;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import ar.edu.huergo.vectorial.calidad.bucher.dto.publication.PublicacionCreateDTO;
+import ar.edu.huergo.vectorial.calidad.bucher.dto.publication.PublicacionResponseDTO;
+import ar.edu.huergo.vectorial.calidad.bucher.entity.publication.Publicacion;
+
+@Component
 public class PublicacionMapper {
-    
+
+    public Publicacion toEntity(PublicacionCreateDTO publicacionCreateDTO) {
+        Publicacion publicacion = new Publicacion();
+        publicacion.setDescripcion(publicacionCreateDTO.getDescripcion());
+        publicacion.setLimiteDias(publicacionCreateDTO.getLimiteDias());
+
+        return publicacion;
+    }
+
+    public PublicacionResponseDTO toDTO(Publicacion publicacion) {
+        if (publicacion == null) {
+            return null;
+        }
+        PublicacionResponseDTO publicacionResponseDTO = new PublicacionResponseDTO();
+
+        publicacionResponseDTO.setTitulo(publicacion.getLibro().getTitulo());
+        publicacionResponseDTO.setDescripcion(publicacion.getDescripcion());
+        publicacionResponseDTO.setLimiteDias(publicacion.getLimiteDias());
+        publicacionResponseDTO.setUsuarioCreador(publicacion.getUsuario().getUsername());
+        publicacionResponseDTO.setFechaCreacion(publicacion.getFechaCreacion());
+        publicacionResponseDTO.setEstadoPublicacion(publicacion.getEstadoPublicacion());
+
+        return publicacionResponseDTO;
+    }
 }
