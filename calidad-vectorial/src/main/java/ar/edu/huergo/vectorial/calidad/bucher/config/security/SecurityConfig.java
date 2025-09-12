3deda702eb1a3c5f94990ccfc8afd1b4e2266fbe
IgnPine <ipineda@alumno.huergo.edu.ar>
2025-09-12 17:22:05 -0300

    correcciones finales

--- calidad-vectorial/src/main/java/ar/edu/huergo/vectorial/calidad/bucher/config/security/SecurityConfig.java
+++ calidad-vectorial/src/main/java/ar/edu/huergo/vectorial/calidad/bucher/config/security/SecurityConfig.java
@@ -48,6 +48,7 @@ public class SecurityConfig {
                 .requestMatchers(HttpMethod.GET, "/publicacion/**").hasAnyRole("ADMIN", "LECTOR")
                 .requestMatchers(HttpMethod.POST, "/publicacion/**").hasAnyRole("ADMIN", "LECTOR")
                 .requestMatchers(HttpMethod.PUT, "/publicacion/**").hasAnyRole("ADMIN", "LECTOR")
+                .requestMatchers(HttpMethod.DELETE, "/publicacion/**").hasAnyRole("ADMIN", "LECTOR")
 
                 .requestMatchers(HttpMethod.GET, "/biblioteca/**").hasAnyRole("ADMIN", "LECTOR")
                 .requestMatchers(HttpMethod.POST, "/biblioteca/**").hasAnyRole("ADMIN", "LECTOR")
