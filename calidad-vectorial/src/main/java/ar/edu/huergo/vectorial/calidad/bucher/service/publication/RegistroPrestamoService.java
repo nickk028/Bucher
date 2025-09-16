3cd1c0808c7f82deb99e7130671fe8c35d941fbc
nickk <ifalcone@alumno.huergo.edu.ar>
2025-09-15 22:07:01 -0300

    Comienzo de Crear Registro Prestamo (Compu no funca :()

---
+++ calidad-vectorial/src/main/java/ar/edu/huergo/vectorial/calidad/bucher/service/publication/RegistroPrestamoService.java
@@ -0,0 +1,35 @@
+package ar.edu.huergo.vectorial.calidad.bucher.service.publication;
+
+import java.time.LocalDate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ar.edu.huergo.vectorial.calidad.bucher.entity.publication.Publicacion;
+import ar.edu.huergo.vectorial.calidad.bucher.entity.publication.RegistroPrestamo;
+import ar.edu.huergo.vectorial.calidad.bucher.entity.security.Usuario;
+import ar.edu.huergo.vectorial.calidad.bucher.repository.publication.PublicacionRepository;
+import ar.edu.huergo.vectorial.calidad.bucher.service.book.LibroService;
+import ar.edu.huergo.vectorial.calidad.bucher.service.security.UsuarioService;
+
+@Service
+public class RegistroPrestamoService {
+
+    @Autowired
+    private PublicacionRepository registroPrestamoRepository;
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @Autowired
+    private LibroService libroService;
+
+    public RegistroPrestamo crearRegistro(Usuario usuario, Publicacion publicacion) {
+        RegistroPrestamo registro = new RegistroPrestamo();
+        registro.setPublicacion(publicacion);
+        registro.setUsuario(usuario);
+        registro.setFechaDevolucion(null);
+        registro.setFechaPrestamo(LocalDate.now());
+        return registro;
+    }
+}
