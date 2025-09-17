d5214b4059213dce9bcea907047f3dc9597f23de
nickk <ifalcone@alumno.huergo.edu.ar>
2025-09-16 21:03:32 -0300

    Devolucion Prestamo + Actualizacion Registro

--- calidad-vectorial/src/main/java/ar/edu/huergo/vectorial/calidad/bucher/repository/publication/RegistroPrestamoRepository.java
+++ calidad-vectorial/src/main/java/ar/edu/huergo/vectorial/calidad/bucher/repository/publication/RegistroPrestamoRepository.java
@@ -1,6 +1,7 @@
 package ar.edu.huergo.vectorial.calidad.bucher.repository.publication;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,4 +9,6 @@ import ar.edu.huergo.vectorial.calidad.bucher.entity.publication.RegistroPrestam
 import ar.edu.huergo.vectorial.calidad.bucher.entity.security.Usuario;
 
 public interface RegistroPrestamoRepository extends JpaRepository<RegistroPrestamo, Long> {
+    List<RegistroPrestamo> findByUsuario(Usuario usuario);
+    Optional<RegistroPrestamo> findByUsuarioAndPublicacionAndFechaDevolucionIsNull(Usuario usuario, ar.edu.huergo.vectorial.calidad.bucher.entity.publication.Publicacion publicacion);
 }
