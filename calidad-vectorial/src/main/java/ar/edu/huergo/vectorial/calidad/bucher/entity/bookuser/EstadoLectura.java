15c74e9f3085d6693778244f7837d7ecc5d7d40b
nickk <ifalcone@alumno.huergo.edu.ar>
2025-09-18 21:34:06 -0300

    Creacion enum estado lecutra bookuser

---
+++ calidad-vectorial/src/main/java/ar/edu/huergo/vectorial/calidad/bucher/entity/bookuser/EstadoLectura.java
@@ -0,0 +1,8 @@
+package ar.edu.huergo.vectorial.calidad.bucher.entity.bookuser;
+// Enum para los estados de lectura de los bookusers
+public enum EstadoLectura {
+    Leyendo,
+    Abandonado,
+    Pendiente,
+    Leído
+}
