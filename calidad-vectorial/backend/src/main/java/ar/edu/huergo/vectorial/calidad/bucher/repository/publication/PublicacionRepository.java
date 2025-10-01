2be1880298b2aec089ed3db6112cd2b31879cd90
Roo <rpodazza@alumno.huergo.edu.ar>
2025-09-30 22:12:48 -0300

    Implementacion de obtenerPublicacionPorCategoria

--- calidad-vectorial/backend/src/main/java/ar/edu/huergo/vectorial/calidad/bucher/repository/publication/PublicacionRepository.java
+++ calidad-vectorial/backend/src/main/java/ar/edu/huergo/vectorial/calidad/bucher/repository/publication/PublicacionRepository.java
@@ -3,11 +3,17 @@ package ar.edu.huergo.vectorial.calidad.bucher.repository.publication;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import ar.edu.huergo.vectorial.calidad.bucher.entity.book.Categoria;
 import ar.edu.huergo.vectorial.calidad.bucher.entity.publication.Publicacion;
 import ar.edu.huergo.vectorial.calidad.bucher.entity.security.Usuario;
 
 public interface PublicacionRepository extends JpaRepository<Publicacion, Long> {
     List<Publicacion> findAllByUsuario(Usuario usuario);
+
+    @Query("SELECT p FROM Publicacion p WHERE :categoria MEMBER OF p.libro.categoria")
+    List<Publicacion> findAllByCategoria(@Param("categoria") Categoria categoria);
 
 }
