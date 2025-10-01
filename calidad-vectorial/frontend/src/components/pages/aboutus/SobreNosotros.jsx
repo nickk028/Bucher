a88764bbf2205b13382f65a6a6833a770a31f8d0
IgnPine <ipineda@alumno.huergo.edu.ar>
2025-10-01 01:25:21 -0300

    implementacion Sobre Nosotros

--- calidad-vectorial/frontend/src/components/pages/aboutus/SobreNosotros.jsx
+++ calidad-vectorial/frontend/src/components/pages/aboutus/SobreNosotros.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import '../../elements/global.css'
+import './SobreNosotros.css';
 
 export const SobreNosotros = () => {
     return (
-        <div>
-            aboutus
-            <Link to="/login">
-                <button>Ir al Login</button>
-            </Link>
-        </div>
+        <section className="hero">
+            <div className="hero__content">
+                <h1 className="hero__title">Bücher</h1>
+                <p className="hero__subtitle">
+                Lleva tu lectura al siguiente nivel,
+                <br/>
+                que lo bueno se comparta</p>
+            </div>
+            <div className="hero__image"></div>
+    </section>
+
+    /*
+    <div>
+        aboutus
+        <Link to="/login">
+            <button>Ir al Login</button>
+        </Link>
+    </div>*/
     )
 }
