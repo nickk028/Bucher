3922e9557df7a44f92250e034823fcc07d2a695a
IgnPine <ipineda@alumno.huergo.edu.ar>
2025-10-01 11:44:57 -0300

    implementacion buttons en Sobre Nosotros + ajustes + footer

--- calidad-vectorial/frontend/src/components/pages/aboutus/SobreNosotros.jsx
+++ calidad-vectorial/frontend/src/components/pages/aboutus/SobreNosotros.jsx
@@ -1,21 +1,32 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import { Button } from '../../elements/buttons/Button';
+import { ButtonGroup } from '../../elements/buttons/ButtonGroup';
+import { Footer } from '../../elements/footer/Footer';
 import '../../elements/global.css'
 import './SobreNosotros.css';
 
 export const SobreNosotros = () => {
     return (
-        <section className="hero">
-            <div className="hero__content">
-                <h1 className="hero__title">Bücher</h1>
-                <p className="hero__subtitle">
-                Lleva tu lectura al siguiente nivel,
-                <br/>
-                que lo bueno se comparta</p>
-            </div>
-            <div className="hero__image"></div>
-    </section>
+        <main>
+            <section className="hero">
+                <div className="hero__content">
+                    <h1 className="hero__title">Bücher</h1>
+                    <p className="hero__subtitle">
+                    Lleva tu lectura al siguiente nivel,
+                    <br/>
+                    que lo bueno se comparta.</p>
+                </div>
+                <div className="hero__image"></div>
+        </section>
+        <ButtonGroup>
+            <Button variant='solapa' color='claro'><Link to="/login">Iniciar Sesión</Link></Button>
+            <Button variant='solapa' color='oscuro'><Link to="/register">Registrarse</Link></Button>
+        </ButtonGroup>
 
+        <Footer></Footer>
+    </main>
+    
     /*
     <div>
         aboutus
