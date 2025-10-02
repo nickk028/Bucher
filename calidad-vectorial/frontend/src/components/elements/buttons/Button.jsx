45b5ad2c216b022083e66bba00d1dec38fcb318e
IgnPine <ipineda@alumno.huergo.edu.ar>
2025-10-01 23:53:11 -0300

    correccion button

--- calidad-vectorial/frontend/src/components/elements/buttons/Button.jsx
+++ calidad-vectorial/frontend/src/components/elements/buttons/Button.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 import '../global.css';
 import './Button.css';
 
-export const Button = ({ children, variant='solapa', color='claro' }) => {
+export const Button = ({ children, variant='solapa', color='claro', to="/" }) => {
+    if (to) {
+    return (
+        <Link to={to} className={`btn btn--${variant} btn--${color}`}>{children}</Link>
+        );
+    }
     return (
         <button className={`btn btn--${variant} btn--${color}`}>{children}</button>
     )
