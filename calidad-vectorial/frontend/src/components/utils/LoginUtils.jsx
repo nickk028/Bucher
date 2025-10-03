a71dbf20e51cbe5057be41d007396f7ab138a635
nickk <ifalcone@alumno.huergo.edu.ar>
2025-10-02 21:45:33 -0300

    Implementacion signal + eliminacion de request anteriores login

--- calidad-vectorial/frontend/src/components/utils/LoginUtils.jsx
+++ calidad-vectorial/frontend/src/components/utils/LoginUtils.jsx
@@ -1,10 +1,11 @@
-export async function loginRequest(username, password) {
+export async function loginRequest(username, password, signal) {
     try {
         const respond = await fetch("http://localhost:8080/auth/login", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ username, password }),
             credentials: "include"
+            ,signal
         });
         return respond;
     } catch (error) {
