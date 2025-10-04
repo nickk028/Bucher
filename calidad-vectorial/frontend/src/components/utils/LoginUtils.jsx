9aa9a8dae4ee94d6fe15ea72812f427d105cda02
Roo <rpodazza@alumno.huergo.edu.ar>
2025-10-04 12:54:51 -0300

    Creacion de LoginUtils

--- calidad-vectorial/frontend/src/components/utils/LoginUtils.jsx
+++ calidad-vectorial/frontend/src/components/utils/LoginUtils.jsx
@@ -1,13 +1,10 @@
+import "./FetchUtils";
+import { fetchData } from "./FetchUtils";
+
 export async function loginRequest(username, password, signal) {
     try {
-        const respond = await fetch("http://localhost:8080/auth/login", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ username, password }),
-            credentials: "include"
-            ,signal
-        });
-        return respond;
+        fetchData()
+        
     } catch (error) {
         return { ok: false, message: "Error de conexión" };
     }
