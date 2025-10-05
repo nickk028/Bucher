6647aef3d9a299e7f127170af3a1c08439b212b3
IgnPine <ipineda@alumno.huergo.edu.ar>
2025-10-05 01:11:05 -0300

    ajustes finales register

--- calidad-vectorial/frontend/src/components/elements/buttons/Button.jsx
+++ calidad-vectorial/frontend/src/components/elements/buttons/Button.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import '../global.css';
 import './Button.css';
 
-export const Button = ({ children, type="button", variant='solapa', color='claro', to, isDisabled, onClick }) => {
+export const Button = ({ children, type="button", variant='solapa', color='claro', to, isDisabled=false, onClick }) => {
     if (to) {
     return (
         <Link to={to} className={`btn btn--${variant} btn--${color}`}>{children}</Link>
