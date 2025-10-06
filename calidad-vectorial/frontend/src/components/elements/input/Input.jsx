db9efd3086fcc8d42ad2bec1c1cbe48772a354a5
nickk <ifalcone@alumno.huergo.edu.ar>
2025-10-06 14:49:13 -0300

    Chusmeo de contraseña + Arreglos

--- calidad-vectorial/frontend/src/components/elements/input/Input.jsx
+++ calidad-vectorial/frontend/src/components/elements/input/Input.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import '../../elements/global.css';
 import './Input.css';
 import ver from '../../../assets/img/ver.png';
@@ -6,20 +6,39 @@ import esconder from '../../../assets/img/esconder.png';
 
 export const Input = ({ children, type, name, value, onChange, onFocus, onBlur }) => {
     const [showPassword, setShowPassword] = useState(false);
+    const inputRef = useRef(null);
 
     const togglePassword = (e) => {
-        e.preventDefault();
+        e.preventDefault(); 
         setShowPassword((inputType) => !inputType);
+
+        if (inputRef.current) {
+            // Selecciona el input si se toca el ojo
+            inputRef.current.focus(); 
+        }
+
+        if (inputType == "password") {
+            // Chusmea la contraseña 
+            window.dispatchEvent(new Event("passwordPeek"));
+        } 
+        if (inputType == "text") {
+            // Cierra los ojos
+            window.dispatchEvent(new Event("passwordFocus"));
+        } 
     };
 
     const inputType = type == "password" && showPassword ? "text" : type;
     return (
         <div className='input-group'>
-            <input className={`input-group__input input-group__input--${type}`} onChange={onChange} onFocus={onFocus} onBlur={onBlur} name={name} id={name} type={inputType} value={value} required />
+            <input className={`input-group__input input-group__input--${type}`} onChange={onChange} ref={inputRef}
+            onFocus={() => {if (inputType === "text") {window.dispatchEvent(new Event("passwordPeek"));} else {window.dispatchEvent(new Event("passwordFocus"));}}}
+            onBlur={onBlur} name={name} id={name} type={inputType} value={value} required />
+            
             <label className='input-group__label' htmlFor={name}> {children} </label>
 
             {type === "password" && (
-                <button type="button" className="input-group__ojo" onMouseDown={togglePassword}>
+                <button type="button" className="input-group__ojo" onMouseDown={togglePassword}  >
+                    
                     {showPassword ? (
                         <img src={esconder} alt="esconder" />
                     ): (
