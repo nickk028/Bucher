5d69a459bcff4e8d2ea68a8919b93169901b4ce6
nickk <ifalcone@alumno.huergo.edu.ar>
2025-10-06 22:05:22 -0300

    implementacion header + logos + publicacion card

--- calidad-vectorial/frontend/src/components/elements/header/Header.jsx
+++ calidad-vectorial/frontend/src/components/elements/header/Header.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import logo from '../../../assets/img/logo.png';
+import logoBucher from '../../../assets/img/logos/logoBucher.png';
+import logoTendencias from '../../../assets/img/logos/logoTendencias.png';
+import logoCrear from '../../../assets/img/logos/logoCrear.png';
+import logoLibro from '../../../assets/img/logos/logoLibro.png';
+import logoUsuario from '../../../assets/img/logos/logoUsuario.png';
+import './Header.css';
 
-export const Header = () => {
-    
-  return (
-    <div>Header</div>
-  )
+const Header = () => {
+    return (
+        <header className="header">
+            <Link to="/index"><img src={logoBucher} alt="Logo de Bücher" className="header__img" /></Link>
+            <Link><img src={logoTendencias} alt="Logo de Tendencias" className="header__img" /></Link>
+            <Link to="/crear-publicacion"><img src={logoCrear} alt="Logo de Crear" className="header__img" /></Link>
+            <Link to="/biblioteca"><img src={logoLibro} alt="Logo de Libro" className="header__img" /></Link>
+            <Link><img src={logoUsuario} alt="Logo de Usuario" className="header__img" /></Link>
+        </header>
+    )
 }
+export default Header
