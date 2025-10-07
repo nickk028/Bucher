4107fde19f9a046473344b638253cedc6ebed07a
IgnPine <ipineda@alumno.huergo.edu.ar>
2025-10-07 15:52:35 -0300

    implementacion fuente + arreglos + color del logo bucher

--- calidad-vectorial/frontend/src/components/pages/publication/Publicacion.jsx
+++ calidad-vectorial/frontend/src/components/pages/publication/Publicacion.jsx
@@ -1,4 +1,4 @@
-import { Link, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { useState, useEffect } from "react";
 import { getData } from '../../utils/FetchUtils';
 import Header from '../../elements/header/Header';
@@ -46,20 +46,20 @@ export const Publicacion = () => {
             <Header />
             <main className='body-pub__content'>
                 <nav className='body-pub__content__nav'></nav>
-            {loading && <p>Cargando...</p>}
-            {publicacion && !loading && (
-                <article className='body-pub__content__publicacion'>
-                    <img className='body-pub__content__publicacion__img' src="https://upload.wikimedia.org/wikipedia/commons/8/88/Map_of_Arrakis_from_Dune_first_edition_dust_jacket.jpg" alt="Foto del libro" />
-                    <div className='.body-pub__content__publicacion__text'>
-                        <h1>{publicacion.titulo}</h1>
-                        <p>{publicacion.descripcion}</p>
-                        <p>{publicacion.estadoPublicacion}</p>
-                        <p>{publicacion.usuarioCreador}</p>
-                        <p>{publicacion.descripcionUsuario}</p>
-                        <p>{error}</p>
-                    </div>
-                </article>
-            )}
+                {loading && <p>Cargando...</p>}
+                {publicacion && !loading && (
+                    <article className='body-pub__content__publicacion'>
+                        <img className='body-pub__content__publicacion__img' src="https://upload.wikimedia.org/wikipedia/commons/8/88/Map_of_Arrakis_from_Dune_first_edition_dust_jacket.jpg" alt="Foto del libro" />
+                        <div className='body-pub__content__publicacion__text'>
+                            <h1 className='body-pub__content__publicacion__text__title'>{publicacion.titulo}</h1>
+                            <p>{publicacion.descripcion}</p>
+                            <p>{publicacion.estadoPublicacion}</p>
+                            <p>{publicacion.usuarioCreador}</p>
+                            <p>{publicacion.descripcionUsuario}descripcion</p>
+                            {/*<p>{error}</p>*/}
+                        </div>
+                    </article>
+                )}
             </main>
         </div>
     )
