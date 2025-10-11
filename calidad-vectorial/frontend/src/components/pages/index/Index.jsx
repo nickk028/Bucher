85640b35cfb9aaf3f0b94b829798f35b5da46ae9
nickk <ifalcone@alumno.huergo.edu.ar>
2025-10-10 22:14:27 -0300

    Implementacion useFetch en login

--- calidad-vectorial/frontend/src/components/pages/index/Index.jsx
+++ calidad-vectorial/frontend/src/components/pages/index/Index.jsx
@@ -1,41 +1,12 @@
 import { Link } from 'react-router-dom';
-import { useState, useEffect } from "react";
-import { getData } from '../../utils/FetchUtils';
+import { useFetch } from '../../utils/FetchUtils';
 import Header from '../../elements/header/Header';
 import PublicacionCard from '../../elements/publication/PublicacionCard';
 import './Index.css';
 import '../../elements/global.css';
 
 export const Index = () => {
-    const [error, setError] = useState("");
-    const [loading, setLoading] = useState(false);
-    const [publicaciones, setPublicaciones] = useState([]);
-
-    const fetchPublicaciones = async (signal) => {
-        try {
-            setLoading(true);
-            const respond = await getData("publicacion", signal);
-            if (respond.ok) {
-                const data = await respond.json();
-                setPublicaciones(data);
-            } else {
-                setError("Error al obtener las publicaciones");
-            }
-        } catch (err) {
-            if (err.name !== 'AbortError') {
-                setError("Hubo un error: " + err.message);
-            }
-        }
-        finally {
-            setLoading(false);
-        }
-    }
-
-    useEffect(() => {
-        const controller = new AbortController();
-        fetchPublicaciones(controller.signal);
-        return () => controller.abort();
-    }, []);
+    const { data : publicaciones, error, loading } = useFetch("publicacion");
 
     return (
         <div className='body-index'>
