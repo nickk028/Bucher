ac8328fb2952e28e7d5258ae97709fe1226ee7a9
IgnPine <ipineda@alumno.huergo.edu.ar>
2025-10-11 20:10:06 -0300

    finalizacion card detalles usuario

--- calidad-vectorial/frontend/src/components/elements/user/UsuarioDetalles.jsx
+++ calidad-vectorial/frontend/src/components/elements/user/UsuarioDetalles.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import logoUsuario from "../../../assets/img/logos/logoUsuario.png"
 import './UsuarioDetalles.css';
 
 export const UsuarioDetalles = ({ children, nombre }) => {
@@ -8,7 +8,7 @@ export const UsuarioDetalles = ({ children, nombre }) => {
                 <h1 className='card-user__content__name'>{nombre}</h1>
                 <p className='card-user__content__description'>{children}</p>
             </div>
-            
+            <img src={logoUsuario} alt="" />
         </div>
     )
 }
