2bc2d3d4f822e8c08b04ca0016138de32d1ee144
IgnPine <ipineda@alumno.huergo.edu.ar>
2025-10-11 20:02:41 -0300

    finalizacion de publiacion

--- calidad-vectorial/frontend/src/components/pages/publication/Publicacion.jsx
+++ calidad-vectorial/frontend/src/components/pages/publication/Publicacion.jsx
@@ -1,9 +1,10 @@
+import { useParams } from "react-router-dom";
 import { useFetch } from '../../utils/FetchUtils';
 import Header from '../../elements/header/Header';
 import Buscador from '../../elements/buscador/Buscador';
 import { UsuarioDetalles } from '../../elements/user/UsuarioDetalles';
+import { Button } from '../../elements/buttons/Button';
 import './Publicacion.css';
-import { useParams } from "react-router-dom";
 
 export const Publicacion = () => {
     /*const [publicacion, setPublicacion] = useState(null);*/
@@ -16,13 +17,17 @@ export const Publicacion = () => {
             <main className='body-pub__content'>
                 <Buscador />
                 <article className='body-pub__content__publicacion'>
-                    <img className='body-pub__content__publicacion__img' src = {publicacion.urlFoto} alt="Foto del libro" />
+                    <aside className="body-pub__content__publicacion__aside">
+                        <img className='body-pub__content__publicacion__img' src = {publicacion.urlFoto} alt="Foto del libro" />
+                        <Button variant="default" color="oscuro">Pedir libro</Button>
+                    </aside>
+
                     <div className='body-pub__content__publicacion__text'>
                         <h1 className='body-pub__content__publicacion__text__title'>{publicacion.titulo}</h1>
                         <p className='body-pub__content__publicacion__text__author'>{publicacion.nombre}</p>
                         <div className='body-pub__content__publicacion__text__item'>
                             <span className='body-pub__content__publicacion__text__item__subtitle'>Descripción del libro</span>
-                            <p>{publicacion.descripcion}</p>
+                            <p className='body-pub__content__publicacion__text__item__parrafo'>{publicacion.descripcion}</p>
                         </div>
                         <div>
                             <span className='body-pub__content__publicacion__text__item__subtitle'>Estado: </span>
@@ -38,7 +43,6 @@ export const Publicacion = () => {
                             <span className='body-pub__content__publicacion__text__item__subtitle'>Fehca de creación: </span>
                             {publicacion.fechaCreacion}
                         </div>
-                        
                         {/*<p>{error}</p>*/}
                     </div>
                 </article>
