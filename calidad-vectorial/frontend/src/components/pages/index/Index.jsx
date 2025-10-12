9328f86361c532567b63c37e58af9b3bc068f97f
IgnPine <ipineda@alumno.huergo.edu.ar>
2025-10-11 23:18:40 -0300

    implementacion css crear publicacion + arreglos

--- calidad-vectorial/frontend/src/components/pages/index/Index.jsx
+++ calidad-vectorial/frontend/src/components/pages/index/Index.jsx
@@ -1,6 +1,5 @@
 import { Link } from 'react-router-dom';
 import { useFetch } from '../../utils/FetchUtils';
-import Header from '../../elements/header/Header';
 import PublicacionCard from '../../elements/publication/PublicacionCard';
 import './Index.css';
 import '../../elements/global.css';
@@ -9,28 +8,26 @@ export const Index = () => {
     const { data : publicaciones, error, loading } = useFetch("publicacion");
 
     return (
-        <>
-            <main className='body-index'>
-                {loading ? (
-                    <p>Cargando...</p>
-                ) : publicaciones.length > 0 ? (
-                    <ul className='body-index__pub-list'>
-                        {publicaciones.map(pub => (
-                            <li className='body-index__pub-list__item' key={pub.id}> <Link to={`/publicacion/${pub.id}`}>
-                                <PublicacionCard
-                                    urlFoto={pub.urlFoto}
-                                    titulo={pub.titulo}
-                                    usuarioNickname={pub.usuarioNickname}
-                                    estadoPublicacion={pub.estadoPublicacion}
-                                    limiteDias={pub.limiteDias}>
-                                </PublicacionCard>
-                            </Link></li>
-                        ))}
-                    </ul>
-                ) : (
-                    <p>{error ? 'No fue posible obtener publicaciones.' : 'No hay publicaciones.'}</p>
-                )}
-            </main>
-        </>
+        <main className='body-index'>
+            {loading ? (
+                <p>Cargando...</p>
+            ) : publicaciones.length > 0 ? (
+                <ul className='body-index__pub-list'>
+                    {publicaciones.map(pub => (
+                        <li className='body-index__pub-list__item' key={pub.id}> <Link to={`/publicacion/${pub.id}`}>
+                            <PublicacionCard
+                                urlFoto={pub.urlFoto}
+                                titulo={pub.titulo}
+                                usuarioNickname={pub.usuarioNickname}
+                                estadoPublicacion={pub.estadoPublicacion}
+                                limiteDias={pub.limiteDias}>
+                            </PublicacionCard>
+                        </Link></li>
+                    ))}
+                </ul>
+            ) : (
+                <p>{error ? 'No fue posible obtener publicaciones.' : 'No hay publicaciones.'}</p>
+            )}
+        </main>
     )
 }
