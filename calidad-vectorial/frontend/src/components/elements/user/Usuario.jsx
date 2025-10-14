dd42c22a7a8b17cdeda63dcfc1bcffc80cd2f31d
nickk <ifalcone@alumno.huergo.edu.ar>
2025-10-13 21:03:15 -0300

    Implementacion logout

--- calidad-vectorial/frontend/src/components/elements/user/Usuario.jsx
+++ calidad-vectorial/frontend/src/components/elements/user/Usuario.jsx
@@ -1,13 +1,23 @@
-import { useFetch } from "../../utils/FetchUtils" 
+import { useFetch, usePost } from "../../utils/FetchUtils" 
 
 export const Usuario = () => {
-    const { data, loading, error } = useFetch("usuario/propio");
+    const { data : respuestaUsuario, loading : loadingUsuario, error : errorUsuario } = useFetch("usuario/propio");
+    const { data : respuestaLogout , error : errorLogout, loading : loadingLogout, execute } = usePost("auth/logout", {});
+    
+    const handleLogout = () => {
+        execute();
+    }
 
     return (
         <div>
-            <p>{JSON.stringify(data, null, 2)}</p>
-            <p>{loading && "Cargando..."}</p>
-            <p>{error}</p>
+            <p>{JSON.stringify(respuestaUsuario, null, 2)}</p>
+            <p>{loadingUsuario && "Cargando Usuario..."}</p>
+            <p>{errorUsuario}</p>
+
+            <button onClick={handleLogout}>Cerrar sesión</button>
+            {loadingLogout && <p>Cerrando sesión...</p>}
+            {errorLogout && <p>{errorLogout}</p>}
+            {respuestaLogout && <p>{JSON.stringify(respuestaLogout, null, 2)}</p>}
         </div>
     );
 }
