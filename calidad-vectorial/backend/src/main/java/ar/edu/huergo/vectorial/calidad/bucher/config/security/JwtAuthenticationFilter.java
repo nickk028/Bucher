2f6b32a0041dee3d45af43809758b0c304973832
nickk <ifalcone@alumno.huergo.edu.ar>
2025-10-14 16:53:05 -0300

    Arreglo logout

--- calidad-vectorial/backend/src/main/java/ar/edu/huergo/vectorial/calidad/bucher/config/security/JwtAuthenticationFilter.java
+++ calidad-vectorial/backend/src/main/java/ar/edu/huergo/vectorial/calidad/bucher/config/security/JwtAuthenticationFilter.java
@@ -1,6 +1,7 @@
 package ar.edu.huergo.vectorial.calidad.bucher.config.security;
 
 import java.io.IOException;
+
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -9,14 +10,13 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import ar.edu.huergo.vectorial.calidad.bucher.service.security.JwtTokenService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-
-import ar.edu.huergo.vectorial.calidad.bucher.service.security.JwtTokenService;
 
 @Component
 @RequiredArgsConstructor
@@ -36,6 +36,13 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     // 4) Continúa la cadena de filtros para que el request llegue al controlador.
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
         FilterChain filterChain) throws ServletException, IOException {
+
+        // Valida rutas publicas
+        String path = request.getRequestURI();
+        if (path.equals("/auth/login") || path.equals("/auth/logout")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
 
         String token = null;
         String username = null;
