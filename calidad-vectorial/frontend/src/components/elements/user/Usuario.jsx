29d172dea3b51536f74a17d1419efc56fb5b4322
IgnPine <ipineda@alumno.huergo.edu.ar>
2025-10-17 16:33:32 -0300

    comienzo de poner lindo el componente de usuario + avatares en public

--- calidad-vectorial/frontend/src/components/elements/user/Usuario.jsx
+++ calidad-vectorial/frontend/src/components/elements/user/Usuario.jsx
@@ -1,4 +1,6 @@
-import { useFetch, usePost } from "../../utils/FetchUtils" 
+import { useFetch, usePost } from "../../utils/FetchUtils"
+import { Button } from "../buttons/Button";
+import './Usuario.css';
 
 export const Usuario = () => {
     const { data : respuestaUsuario, loading : loadingUsuario, error : errorUsuario } = useFetch("usuario/propio");
@@ -8,7 +10,28 @@ export const Usuario = () => {
         execute();
     }
 
+    const avatarUsuario = respuestaUsuario?.avatar?.toLowerCase() + ".png";
+
     return (
+        <aside className="aside-user">
+            <img src={`/assets/img/avatares/${avatarUsuario}`} alt="" />
+
+            <h1>{respuestaUsuario.username}</h1>
+
+            <p>{respuestaUsuario.pronombres}</p>
+
+            <p>{respuestaUsuario.descripcion}</p>
+
+            <div className="aside-user__buttons">
+            <Button variant="default" color="oscuro">Editar perfil</Button>
+
+            <Button variant="default" color="rojo">Cerrar sesión</Button>
+            </div>
+        </aside>
+
+        
+    );
+    {/*
         <div>
             <p>{JSON.stringify(respuestaUsuario, null, 2)}</p>
             <p>{loadingUsuario && "Cargando Usuario..."}</p>
@@ -18,6 +41,5 @@ export const Usuario = () => {
             {loadingLogout && <p>Cerrando sesión...</p>}
             {errorLogout && <p>{errorLogout}</p>}
             {respuestaLogout && <p>{JSON.stringify(respuestaLogout, null, 2)}</p>}
-        </div>
-    );
-}
+        </div>*/}
+};
