ca401de890fe2b9f18d093695ba408f70ce16bca
nickk <ifalcone@alumno.huergo.edu.ar>
2025-10-20 23:46:46 -0300

    Cambio coming soon css

--- calidad-vectorial/frontend/src/components/pages/Errors/ComingSoon.jsx
+++ calidad-vectorial/frontend/src/components/pages/Errors/ComingSoon.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 export const ComingSoon = () => {
     return (  
         <div className="coming-soon">
-            <div className="cs-box">
+            <div className="coming-soon-box">
                 <h1>Coming Soon</h1>
-                <Link to="/index" className="cs-link">Volver al Inicio</Link>
+                <Link to="/index" className="coming-soon-link">Volver al Inicio</Link>
             </div>
         </div>
     )
