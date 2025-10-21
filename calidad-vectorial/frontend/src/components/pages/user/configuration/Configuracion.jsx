2745be2ae92ab69c0408a0fdb203f78dacae0b98
nickk <ifalcone@alumno.huergo.edu.ar>
2025-10-20 22:56:30 -0300

    Implementacion configuracion en LocalStorage

--- calidad-vectorial/frontend/src/components/pages/user/configuration/Configuracion.jsx
+++ calidad-vectorial/frontend/src/components/pages/user/configuration/Configuracion.jsx
@@ -1,10 +1,32 @@
-import { Usuario } from "../../../elements/user/Usuario"
+import { useEffect, useState } from 'react';
+import { validarExisteConfig, setConfig } from '../../../utils/ConfigUtils';
+import { Usuario } from '../../../elements/user/Usuario';
 
 export const Configuracion = () => {
+    // Carga/ crea la configuración al montar
+    const [configuracion, setConfiguracion] = useState(() => validarExisteConfig());
+
+    // Actualiza cada vez que cambia configuracion
+    useEffect(() => {
+        setConfig(configuracion);
+    }, [configuracion]);
+    
+    const onConfigChange = (e) => {
+        const { name, checked } = e.target;   // siempre boolean
+        setConfiguracion(prev => ({ ...prev, [name]: checked }));
+    };
+
     return (
         <div>
-            <Usuario/>
-            <h1>Configuración</h1>
+            <Usuario />
+            <label>
+                {/* (!!) Transforma cualquier valor a boolean */}
+                <input type="checkbox" checked={!!configuracion.buchy} onChange={onConfigChange} name="buchy"/>
+                Activar "buchy"
+            </label>
+
+            {/* JSON guardado */}
+            <p>{JSON.stringify(configuracion, null, 2)}</p>
         </div>
-    )
-}
+    );
+};
