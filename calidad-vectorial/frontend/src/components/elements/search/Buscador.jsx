5f6211ba19977fa8c54592e7577e9be8cb309cdb
nickk <ifalcone@alumno.huergo.edu.ar>
2025-11-11 19:29:38 -0300

    Implementacion buscador funcional

--- calidad-vectorial/frontend/src/components/elements/search/Buscador.jsx
+++ calidad-vectorial/frontend/src/components/elements/search/Buscador.jsx
@@ -1,15 +1,36 @@
-import { Input } from "../input/Input";
 import lupa from "../../../assets/img/lupa.png";
 import "./Buscador.css";
+import { useFetch } from "../../utils/FetchUtils";
+import { useState } from "react";
+import { Autocompletar } from "../autocomplete/Autocompletar";
+import { useNavigate } from "react-router-dom";
 
 const Buscador = () => {
+    const { data : dataLibros , errorLibros , loadingLibros  } = useFetch("libro/todos");
+    const [titulo, setTitulo] = useState("");
+    const navigate = useNavigate();
+
+    const handleBuscarLibro = (e) => {
+        e.preventDefault();
+        const id = dataLibros.find(libro => libro.titulo === titulo)?.id || '';
+        navigate(`/libros/${id}`);
+    }
+
     return (
         <nav className="buscador">
-            <div className="buscador__input">
-                <Input type="text" name="buscador" placeholder="Buscar..." ></Input>
-                <label htmlFor="buscador"><img className="buscador__input__lupa" src={lupa} alt="Lupa" /></label>
-            </div>
-            
+            <form className="buscador__input" onSubmit={handleBuscarLibro}>
+                <Autocompletar
+                    options={dataLibros ? dataLibros.map(libro => [libro.urlFoto ,libro.titulo]) : []}
+                    type = "text"
+                    tipo = "doble"
+                    imgHeight = "100px"
+                    imagWidth = "60px"
+                    value = {titulo}
+                    name = "titulo"
+                    onChange = {e => setTitulo(e.target.value)}>
+                </Autocompletar>
+                <button type = "submit" style={{border: "hidden"}}><img className="buscador__input__lupa" src={lupa} alt="Lupa" /></button>
+            </form>
         </nav>
     )
 }
