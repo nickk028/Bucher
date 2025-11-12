81cb64283d3999f0589ffce67f543a55ac3fbbbf
IgnPine <ipineda@alumno.huergo.edu.ar>
2025-11-12 01:46:50 -0300

    Implementacion barra de seguridad de password

--- calidad-vectorial/frontend/src/components/pages/register/Register.jsx
+++ calidad-vectorial/frontend/src/components/pages/register/Register.jsx
@@ -2,16 +2,21 @@ import { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { loginRequest, validarSeguridadPassword } from "../../utils/LoginUtils";
+import { useBook } from "../../../context/LibroContexto";
 import "./Register.css";
 import { AuthBox } from "../../elements/authbox/AuthBox";
 import { Button } from "../../elements/buttons/Button";
 import { Input } from "../../elements/input/Input";
 import { postData } from "../../utils/FetchUtils";
 import { LibroAnimado } from "../../elements/animatedbook/LibroAnimado";
+import { BarraPassword } from "../../elements/bar/BarraPassword";
 
 export const Register = () => {
     const navigate = useNavigate();
     const controllerRef = useRef(null);
+
+    const { libroMensaje, setLibroMensaje } = useBook();
+
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [verificationPassword, setVerificationPassword] = useState("");
@@ -72,6 +77,10 @@ export const Register = () => {
         };
     }, []);
 
+    useEffect(() => {
+        setOjoQueHabla(Math.floor(Math.random() * 3));
+    }, [libroMensaje])
+
     const handleRegister = async (evento) => {
         evento.preventDefault();
         if (controllerRef.current) {
@@ -81,7 +90,7 @@ export const Register = () => {
 		// Nuevo controller para la nueva request
 		const controller = new AbortController();
 		controllerRef.current = controller; // guarda el controller en la referencia
-        setError("");
+        setLibroMensaje("");
 
         try {
             const respond = await postData("usuario/registrar", {
@@ -95,14 +104,14 @@ export const Register = () => {
                 // Login automatico sin guardar respond
                 navigate("/index");
             } else {
-                const text = await respond.text();
+                const text = await respond.json();
                 setPaso(1);
-                setError(text);
+                setLibroMensaje(text.detail);
                 setOjoQueHabla(Math.floor(Math.random() * 3));
             }
             setOjoQueHabla(Math.floor(Math.random() * 3));
         } catch (error) {
-            setError("Error de conexión");
+            setLibroMensaje("Error de conexión");
             setOjoQueHabla(Math.floor(Math.random() * 3));
         }
     };
@@ -110,25 +119,40 @@ export const Register = () => {
     return (
         <div className="body-register">
             <div className="body-login__grupo-ojos">
-                <LibroAnimado variant="grande" color="rojo" mensaje={error} mostrarMensaje={ojoQueHabla === 0}>
+                <LibroAnimado variant="grande" color="rojo" mensaje={libroMensaje} mostrarMensaje={ojoQueHabla === 0}>
                     Bü
                 </LibroAnimado>
-                <LibroAnimado variant="chico" color="azul" mensaje={error} mostrarMensaje={ojoQueHabla === 1}>
+                <LibroAnimado variant="chico" color="azul" mensaje={libroMensaje} mostrarMensaje={ojoQueHabla === 1}>
                     ch
                 </LibroAnimado>
-                <LibroAnimado variant="medio" color="amarillo" mensaje={error} mostrarMensaje={ojoQueHabla === 2}>
+                <LibroAnimado variant="medio" color="amarillo" mensaje={libroMensaje} mostrarMensaje={ojoQueHabla === 2}>
                     er
                 </LibroAnimado>
             </div>
             {paso == 1 && (
                 <AuthBox titulo="Registrarse"
                 botonDer={
-                    <Button variant="default" color="oscuro" isDisabled={isDisabled} onClick={() => {if (validarSeguridadPassword(password) === 5 || verificationPassword === password) {setPaso(paso + 1)}}}>Siguiente</Button>
+                    <Button variant="default" color="oscuro" isDisabled={isDisabled}
+                    onClick={() => {
+                            if (verificationPassword === password) {
+                                if (validarSeguridadPassword(password) === 5) {
+                                    setPaso(paso + 1)
+                                } else {
+                                    setLibroMensaje("La contraseña no es del todo segura.");
+                                }
+                            } else {
+                                setLibroMensaje("Las contraseñas deben ser iguales.");
+                            }
+                        }
+                    }>
+                        Siguiente
+                    </Button>
                 }
                 linkExtra={
                     <Link to="/login">¿Ya tienes una cuenta? ¡Inicia sesión!</Link>
                 }>
                     <Input type="text" value={username} name="username" onChange={e => setUsername(e.target.value)} autoComplete="on">Email</Input>
+                    <BarraPassword password={password}></BarraPassword>
                     <Input type="password" value={password} name="password" onChange={e => setPassword(e.target.value)} autoComplete="on">Contraseña</Input>
                     <Input type="password" value={verificationPassword} name="verificationPassword" onChange={e => setVerificationPassword(e.target.value)}>Confirmar contraseña</Input>
                 </AuthBox>
@@ -137,7 +161,7 @@ export const Register = () => {
             {paso == 2 && (
                 <AuthBox titulo="Ya casi..."
                 botonDer={
-                    <Button variant="default" color="oscuro" isDisabled={isDisabled} onClick={() => {if (parseInt(fechaNacimiento.split("-")) < yearActual && parseInt(fechaNacimiento.split("-")) > yearActual - 100) {setPaso(paso + 1)}}}>Siguiente</Button>
+                    <Button variant="default" color="oscuro" isDisabled={isDisabled} onClick={() => {if (parseInt(fechaNacimiento.split("-")) < yearActual && parseInt(fechaNacimiento.split("-")) > yearActual - 100) {setPaso(paso + 1)} else {setLibroMensaje("La fecha de nacimiento debe ser válida.")}}}>Siguiente</Button>
                 }
                 botonIzq={
                     <Button variant="default" color="oscuro" onClick={() => setPaso(paso - 1)}>Atrás</Button>
@@ -162,4 +186,4 @@ export const Register = () => {
             )}
 		</div>
     );
-}
+};
