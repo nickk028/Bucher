c954e3db3110737f1bcb1c26490700191afb0232
IgnPine <ipineda@alumno.huergo.edu.ar>
2025-11-12 22:49:29 -0300

    arreglos varios

--- calidad-vectorial/frontend/src/components/elements/user/UsuarioDetalles.jsx
+++ calidad-vectorial/frontend/src/components/elements/user/UsuarioDetalles.jsx
@@ -1,14 +1,19 @@
 import logoUsuario from "../../../assets/img/logos/logoUsuario.png"
 import "./UsuarioDetalles.css";
 
-export const UsuarioDetalles = ({ children, nombre }) => {
+export const UsuarioDetalles = ({ children, nombre, foto }) => {
     return (
         <div className="card-user">
             <div className="card-user__content">
                 <h1 className="card-user__content__name">{nombre}</h1>
                 <p className="card-user__content__description">{children}</p>
             </div>
-            <img src={logoUsuario} alt="Imagen del usuario"/>
+
+            {foto ? (
+                <img className="card-user__foto" src={foto} alt="Imagen del usuario"/>
+            ) : (
+                <img className="card-user__foto" src={logoUsuario} alt="Imagen del usuario"/>
+            )}
         </div>
-    )
-}
+    );
+};
