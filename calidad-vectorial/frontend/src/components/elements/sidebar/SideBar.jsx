5833f2b4ddc4135b5aba7a003cddfffc45222684
IgnPine <ipineda@alumno.huergo.edu.ar>
2026-09-02 21:02:18 -0300

    imlementacion logout + arreglos

--- calidad-vectorial/frontend/src/components/elements/sidebar/SideBar.jsx
+++ calidad-vectorial/frontend/src/components/elements/sidebar/SideBar.jsx
@@ -1,6 +1,17 @@
+import { useFetch, usePost } from "../../utils/FetchUtils";
+import { useNavigate } from "react-router-dom";
+import Logout from "../../../assets/img/icons/configuracion/logout.svg?react";
 import"./SideBar.css";
 
 export const SideBar = ({children, titulo}) => {
+    const navigate = useNavigate();
+    const { data: respuestaLogout, error: errorLogout, loading: loadingLogout, execute : executeLogout} = usePost("auth/logout");
+
+    const handleLogout = async () => {
+        await executeLogout();
+        navigate("/");
+    };
+
     return (
         <nav className="sidebar">
             <div className="sidebar__content">
@@ -8,6 +19,12 @@ export const SideBar = ({children, titulo}) => {
                     <h1>{titulo}</h1>
                     <div className="sidebar__cards">
                         {children}
+                        <div className="sidebar__content__card" onClick={handleLogout}>
+                            <div className="sidebar__content__card__logout">
+                                <Logout />
+                            </div>
+                            <p>Cerrar sesión</p>
+                        </div>
                     </div>
                 </div>
                 <div className="sidebar__content__footer">
